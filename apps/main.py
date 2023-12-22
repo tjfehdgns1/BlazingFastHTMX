e6a7aceb55c15cfaa8fa6c3703a4dcc171658bd6
Seolman <tjfehdgns@gmail.com>
2023-12-22 16:44:16 +0900

    unfinish adding db

--- apps/main.py
+++ apps/main.py
@@ -1,21 +1,36 @@
 from typing import Optional
-from fastapi import FastAPI, Request, Header
+from fastapi import FastAPI, Request, Header, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from . import models
+from .database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/index/", response_class=HTMLResponse)
-async def read_root(request: Request, hx_request: Optional[str] = Header(None)):
-    films = [
-        {"name": "Star Wars: 2", "year": "2019", "rating": "3"},
-        {"name": "Carribian Pirates", "year": "2009", "rating": "8"},
-        {"name": "Shit", "year": "1989", "rating": "7"},
-    ]
+async def read_root(
+    request: Request,
+    hx_request: Optional[str] = Header(None),
+    db: Session = Depends(get_db),
+):
+    films = db.query(models.Film).all()
+    print(films)
     context = {"request": request, "films": films}
 
     if hx_request:
